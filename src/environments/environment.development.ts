ae13a38887112ca865e770037c5f457bb0fc2804
zhburr <mianzhb@gmail.com>
2023-09-24 17:23:37 +0500

    enviroment changes

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,11 @@
 export const environment = {
-  production: false,
-  baseURL: 'https://b2b-nest.vercel.app/',
+  production: true,
+  baseURL: 'http://141.136.42.56:3000/',
+  // baseURL: 'http://localhost:3000/',
   name: 'B2B Direct',
   jwtSk: 'B2B-Direct-Access-Token-Secret-key',
+  paypalClientId:
+    'Ab1kZ3FR1edTpa215RDVDPG8G97xLVnaZEIHotaFOsfO_w2Q-wGX_eBaIqFOv_hwZIGewKyD1sDk5ErW',
+  paypalSecretKey:
+    'EHzscacHOnlw637D-tySFAM5ipoxzPHgJRSD3CPvZf34zLscffrRjKDqiCffyTxeSwIHXQlQ09Fq5msu',
 };
