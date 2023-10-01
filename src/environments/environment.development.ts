062975102b39464ea3d31baea8fa0008a580861d
zhburr <mianzhb@gmail.com>
2023-10-01 11:26:49 +0500

    changes

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,7 +1,8 @@
 export const environment = {
   production: true,
   // baseURL: 'https://141./136.42.56:3000/',
-  baseURL: 'https://dev.b2bdirect.co.uk/',
+  // baseURL: 'https://dev.b2bdirect.co.uk/',
+  baseURL: 'https://prod.b2bdirect.co.uk:3000/',
   // baseURL: 'http://localhost:3000/',
   name: 'B2B Direct',
   jwtSk: 'B2B-Direct-Access-Token-Secret-key',
