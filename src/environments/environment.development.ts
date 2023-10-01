7db58de21b5c2963929ce7a8c09502ffa5fd19cb
zhburr <mianzhb@gmail.com>
2023-10-01 11:44:24 +0500

    aaaaa

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  baseURL: 'http://141./136.42.56:3000/',
+  baseURL: 'http://141.136.42.56:3000/',
   // baseURL: 'https://dev.b2bdirect.co.uk/',
   // baseURL: 'http://prod.b2bdirect.co.uk:3000/',
   // baseURL: 'http://localhost:3000/',
